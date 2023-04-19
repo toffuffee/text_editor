43714add3c9dcfa4f3c479ce89a8fa1159de1ef9
= <=>
2023-04-19 17:03:00 +0300

    Text editor new

--- index.js
+++ index.js
@@ -1,41 +1,55 @@
 (function(){ 
-  // var select = document.querySelector('#fontSie');
-
-  // select.addEventListener('change',function(){
-  //       console.log(this.value)
-  //       document.execCommand( 'fontSize',false,this.value);
-
-  //       let content = document.querySelector(".contentibale").innerHTML;
-  //       document.querySelector("#code-content").value = content;
-
-  // });
-
   document.querySelector(".contentibale").addEventListener("keyup", function(){
     let content = this.innerHTML
     document.querySelector("#code-content").value = content;
   });
 
-  document.querySelector(".fontStyleBold").addEventListener("click", function(){
-    document.execCommand('bold',false,null)
+  document.querySelector(".toolbar-color").addEventListener("input", function(){
+    let val = this.value;
+    document.execCommand('styleWithCSS', false, true);
+    document.execCommand('foreColor', false, val);
+    document.execCommand('styleWithCSS', false, false);
+
     let content = document.querySelector(".contentibale").innerHTML;
     document.querySelector("#code-content").value = content;
   })
 
-  document.querySelector(".fontStyleItalic").addEventListener("click", function(){
+  document.querySelector(".font-style-bold").addEventListener("click", function(){
+    document.execCommand('bold',false,null)
+    
+    let content = document.querySelector(".contentibale").innerHTML;
+    document.querySelector("#code-content").value = content;
+  })
+
+  document.querySelector(".font-style-italic").addEventListener("click", function(){
     document.execCommand('italic',false,null);
     let content = document.querySelector(".contentibale").innerHTML;
     document.querySelector("#code-content").value = content;
   })
 
-  document.querySelector(".fontStyleUnderline").addEventListener("click", function(){
-    document.execCommand( 'underline',false,null);
+  document.querySelector(".font-style-underline").addEventListener("click", function(){
+    document.execCommand('underline',false,null);
     let content = document.querySelector(".contentibale").innerHTML;
     document.querySelector("#code-content").value = content;
   })
 
- 
+  document.querySelector(".font-style-left").addEventListener("click", function(){
+    document.execCommand('justifyLeft', false, null);
+    let content = document.querySelector(".contentibale").innerHTML;
+    document.querySelector("#code-content").value = content;
+  })
+
+  document.querySelector(".font-style-center").addEventListener("click", function(){
+    document.execCommand('justifyCenter', false, null);
+    let content = document.querySelector(".contentibale").innerHTML;
+    document.querySelector("#code-content").value = content;
+  })
+
+  document.querySelector(".font-style-right").addEventListener("click", function(){
+    document.execCommand('justifyRight', false, null);
+    let content = document.querySelector(".contentibale").innerHTML;
+    document.querySelector("#code-content").value = content;
+  })
 
 })();
-
-
 
